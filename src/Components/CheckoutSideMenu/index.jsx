f42a52b0bf5d78618be214797c9cbefaf287fb73
n-skot <ricardo.nskot@gmail.com>
2024-06-21 02:54:06 -0600

    total y botones de checkout, eliminar

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -9,7 +9,7 @@ const CheckuotSideMenu = () => {
     console.log("cart products: ", context.cartProducts);
 
     return (
-        <aside className={`${context.isChekoutSideMenu ? 'flex' : 'hidden'} scrollable-cards checkout-side-menu flex-col fixed bg-white right-0 border border-black rounded-lg`}>
+        <aside className={`${context.isChekoutSideMenu ? 'flex' : 'hidden'} overflow-auto checkout-side-menu flex-col fixed bg-white right-0 border border-black rounded-lg`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>My Order</h2>
                 <XMarkIcon className='size-8 cursor-pointer' onClick={() => context.closeChekoutSideMenu()} />
@@ -17,9 +17,13 @@ const CheckuotSideMenu = () => {
             <div className="px-3">
                 {
                     context.cartProducts.map(product => (
-                        <OrderCard key={product.product} title={product.name} imageUrl={product.image} price={product.price} />
+                        <OrderCard key={product.product} productId={product.product} title={product.name} imageUrl={product.image} price={product.price} />
                     ))
                 }
+            </div>
+            <div className="px-3">
+                <h3 className="text-lg font-medium mt-6">Total: ${context.cartProducts.reduce((total, product) => total + product.price, 0)}</h3>
+                <button className="w-full py-3 text-white font-medium bg-blue-500 rounded-lg">Checkout</button>
             </div>
         </aside>
     )
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,7 +1,15 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { useContext } from "react";
+import { ContextCart } from "../../Context";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 const OrderCard = props => {
-    const { title, imageUrl, price } = props;
+    const { productId, title, imageUrl, price } = props;
+    const context = useContext(ContextCart);
+    const handleDelete = (productId) => {
+        context.setCartProducts(context.cartProducts.filter(product => product.product!==productId))
+        context.setCount(context.count - 1);
+    };
+
     return (
         <div className="flex justify-between items-center mb-3">
             <div className="flex items-center gap-2">
@@ -12,7 +20,7 @@ const OrderCard = props => {
             </div>
             <div className="flex items-center gap-2">
                 <p className="text-lg font-medium">{price}</p>
-                <XMarkIcon className="h-6 w-6 text-black cursor-pointer" />
+                <TrashIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => handleDelete(productId)} />
             </div>
         </div>
     )
